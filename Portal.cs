f3f1152593837c2dc213df8dca27a3c5ccb72035
Wolfsgesicht00 <yannik.duplitzer@gmail.com>
2025-03-16 14:13:16 +0100

    Portal Basisstruktur
    Sollte enthalten aktuell nicht testbar: Teleportation, erschaffen neuer Portale, Max Portallimit von 6, Kommunikation der Portale (Partnerportal jeweils 1-2, 3-4, 5-6)

--- Portal.cs
+++ Portal.cs
@@ -6,19 +6,27 @@ public partial class Portal : RigidBody2D
 private double x = 0;
 private double y = 0; 
 private double radiants;
+private int portalType = 0;
 private Vector2 portalVelocity;
 private CharacterBody2D player;
+private Node2D controller;
 
 	public override void _Ready()
 	{
 		GlobalPosition = new Vector2(0, -400);
 		player = GetNode<CharacterBody2D>("/root/Game/Player");
-  		
 
 	}
 
 	public override void _Process(double delta)
 	{
 		//set_contact_monitor(true);
+	}
+
+	public int GetPortalType(){
+		return this.portalType;
+	}
+	public void SetPortalType(int Type){
+		portalType = Type;
 	}
 }
---
+++ PortalController.cs
@@ -0,0 +1,64 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class PortalController : Node2D
+{
+	private Portal newportal;
+	[Export] public PackedScene PortalSpawner;
+	List<Portal> portale;
+	private CharacterBody2D player;
+	private int touchedPortal;
+	public override void _Ready()
+	{
+		CallDeferred(nameof(SpawnPortal));
+		portale = new List<Portal>();
+		
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+
+	public void SpawnPortal(){
+		if (GetPortalCount() > 5){
+			RigidBody2D delPortal = portale[0]; 
+			delPortal.QueueFree(); 
+			portale.RemoveAt(0);
+			delPortal = portale[0]; 
+			delPortal.QueueFree(); 
+			portale.RemoveAt(0);
+		}
+		else{
+			newportal = PortalSpawner.Instantiate<Portal>();
+			GetParent().AddChild(newportal); 
+			portale.Add(newportal);
+			newportal.SetPortalType(portale.IndexOf(newportal));}
+	}
+	
+
+	public String GetPartnerPortal(int touchedportal){
+		switch(touchedportal){
+			case 0: return portale[1].GetPath();
+			case 1: return portale[0].GetPath();
+			case 2: return portale[3].GetPath();
+			case 3: return portale[2].GetPath();
+			case 4: return portale[5].GetPath();
+			case 5: return portale[4].GetPath();
+			default: return "Error at Portal Index";
+		}
+	}
+	
+	public int GetPortalCount(){
+		return portale.Count;
+	}
+	public void PortalTouched(int portalId){
+		touchedPortal = portalId;
+		Teleport();
+	}
+	public void Teleport(){
+		RigidBody2D partnerPortal = GetNode<RigidBody2D>(GetPartnerPortal(touchedPortal));
+		RigidBody2D rigidTouchedPortal = portale[touchedPortal];
+		player.SetDeferred("GlobalPosition", new Vector2(partnerPortal.GlobalPosition.Y,partnerPortal.GlobalPosition.X));
+	}
+}
