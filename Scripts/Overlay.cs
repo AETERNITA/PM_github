198829f2dd911997afab383761d5cae15728dd8c
huntingblizzard <78653910+huntingblizzard@users.noreply.github.com>
2025-05-15 18:02:24 +0200

    lights in: projectile, portals, items; portalgun and projectile sfx, stopwatch, point display, better portalspawn sfx

--- Scripts/Overlay.cs
+++ Scripts/Overlay.cs
@@ -1,13 +1,17 @@
 using Godot;
 using System;
 
-public partial class AudioSlider : CanvasLayer
+public partial class Overlay : CanvasLayer
 {
 	private Label Master_Label;
 	
 	private int Master_Index;
 	
 	private HSlider Master_Slider;
+    private Label Stopwatch;
+    private Label Points;
+
+    private double time = 0;
 	
 	public override void _Ready(){
 		Master_Label = GetNode<Label>("Master_Label");
@@ -15,9 +19,21 @@ public partial class AudioSlider : CanvasLayer
 		Master_Index = AudioServer.GetBusIndex("Master");
 		
 		Master_Slider = GetNode<HSlider>("Master_Slider");
+
+        Stopwatch = GetNode<Label>("Stopwatch");
+        Stopwatch.Text = "Time: 0";
+
+        Points = GetNode<Label>("Points");
+        Points.Text = "Points: 0";
 		
 		Master_Slider.Value = 0.5;
 	}
+
+    public override void _Process(double delta)
+    {
+        time = time + delta;
+        Stopwatch.Text = "Time:" + Math.Round(time,2).ToString();
+    }
 	
 public void _on_master_slider_value_changed(float myFloat){
 	
