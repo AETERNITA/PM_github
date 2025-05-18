a1ee47c267b870d1497a03ea11fcdbe250c730f8
huntingblizzard <78653910+huntingblizzard@users.noreply.github.com>
2025-05-18 16:10:05 +0200

    new parent class for all enemies and the player, temp enemy that damages the player (and all other objects that inherit from the new class) and has the godot symbol;

---
+++ Scripts/parent_scipts/GenericCharacterClass.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using static Godot.GD;
+
+public partial class GenericCharacterClass : CharacterBody2D
+{
+    public virtual void take_damage(double damage)
+    {
+
+    }
+}
