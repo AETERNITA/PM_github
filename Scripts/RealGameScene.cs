8cbb9faceb9febbc4dcdc999fc3be263857cf850
huntingblizzard <78653910+huntingblizzard@users.noreply.github.com>
2025-06-21 15:52:50 +0200

    lvlgen, interactables and enemy updates for that

--- Scripts/RealGameScene.cs
+++ Scripts/RealGameScene.cs
@@ -5,5 +5,15 @@ using static Godot.GD;
 
 public partial class RealGameScene : Node2D
 {
-
+    public void SpawnEnemy(Vector2 EnemyPosition, string EnemyType)
+    {
+        var scene = Load<PackedScene>(EnemyType);
+        var instance = scene.Instantiate();
+        Node2D instance2D = instance as Node2D;
+        if (instance2D != null)
+        {
+            AddChild(instance2D);
+            instance2D.Position = EnemyPosition;
+        }
+    }
 }
