9690cb106b96706e21cd5c8722601e30a25dbe33
huntingblizzard <78653910+huntingblizzard@users.noreply.github.com>
2025-07-04 20:20:04 +0200

    projectile rework to work with tilemaplayers

--- Scripts/Projectilenew.cs
+++ Scripts/Projectilenew.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class Projectilenew : Area2D
+public partial class Projectilenew : CharacterBody2D
 {
 	private CharacterBody2D player;
 	private Sprite2D sprite;
@@ -12,6 +12,7 @@ public partial class Projectilenew : Area2D
 	private Vector2 lastPos;
 	private PortalController portalController;
 	private RayCast2D ray;
+	private KinematicCollision2D LastRegisteredCollision;
 	[Export] private AudioStreamPlayer shoot_sfx;
 	[Export] private AudioStreamPlayer2D flying_sfx;
 	// Called when the node enters the scene tree for the first time.
@@ -33,13 +34,22 @@ public partial class Projectilenew : Area2D
 		if (projectileActive == true)
 		{
 			this.GlobalPosition = new Vector2(GlobalPosition.X + Mathf.Cos(direction) * speed, GlobalPosition.Y + Mathf.Sin(direction) * speed);
+			MoveAndSlide();
+
+			if (GetLastSlideCollision() != null && GetLastSlideCollision() != LastRegisteredCollision)
+			{
+				LastRegisteredCollision = GetLastSlideCollision();
+				_on_body_entered(new StaticBody2D());
+			}
 		}
-		
+
 		if (Input.IsActionJustPressed("controller_shoot_portal"))
 		{
 			Shoot_Controller(gun.GlobalPosition);
 		}
+
 		
+
 	}
 
 	public void Shoot(Vector2 playerPos)
@@ -55,7 +65,8 @@ public partial class Projectilenew : Area2D
 			flying_sfx.Play();
 		}
 	}
-	public void Shoot_Controller(Vector2 playerPos){
+	public void Shoot_Controller(Vector2 playerPos)
+	{
 		if (true)
 		{
 
@@ -68,7 +79,7 @@ public partial class Projectilenew : Area2D
 			flying_sfx.Play();
 		}
 	}
-	
+
 	public void EndShot(float a)
 	{
 		sprite.Visible = false;
@@ -78,7 +89,8 @@ public partial class Projectilenew : Area2D
 		flying_sfx.Stop();
 	}
 
-	public void AssignReferences(){
+	public void AssignReferences()
+	{
 		portalController = GetNode<PortalController>("/root/Game/PortalController");
 		player = GetNode<CharacterBody2D>("/root/Game/Player");
 		sprite = GetNode<Sprite2D>("Sprite2D");
@@ -89,23 +101,23 @@ public partial class Projectilenew : Area2D
 
 	private void RotateProjectileToMouse()
 	{
-			Vector2 mousePosition = GetGlobalMousePosition();
-			Vector2 directionToMouse = mousePosition - GlobalPosition;
-			float angle = directionToMouse.Angle();
-			direction = angle;
+		Vector2 mousePosition = GetGlobalMousePosition();
+		Vector2 directionToMouse = mousePosition - GlobalPosition;
+		float angle = directionToMouse.Angle();
+		direction = angle;
 	}
-	
+
 	private void RotateProjectileToControllerDirection()
 	{
 		float x = Input.GetActionStrength("right_stick_right") - Input.GetActionRawStrength("right_stick_left");
 		float y = Input.GetActionStrength("right_stick_down") - Input.GetActionRawStrength("right_stick_up");
 
-        Vector2 Controller_direction = new Vector2(x, y);
+		Vector2 Controller_direction = new Vector2(x, y);
 
 		float angle = Controller_direction.Angle();
 		direction = angle;
 	}
-	
+
 	public void _on_body_entered(Node2D body)
 	{
 		if (body is StaticBody2D)
@@ -118,14 +130,18 @@ public partial class Projectilenew : Area2D
 
 			if (ray.IsColliding())
 			{
-				
+
 				Vector2 normal = ray.GetCollisionNormal();
 				a1 = Mathf.Atan2(normal.Y, normal.X);
 
 			}
-			
+
 			EndShot(a1);
-		
+
 		}
 	}
+
+
+
+
 }
