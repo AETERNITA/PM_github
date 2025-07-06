3cb8b7cb3db9a6efc7b499cfb3fd84a2edae668f
huntingblizzard <78653910+huntingblizzard@users.noreply.github.com>
2025-07-06 10:06:15 +0200

    new explosive wizard insect

--- Scripts/Enemies/WizardInsectExplosive.cs
+++ Scripts/Enemies/WizardInsectExplosive.cs
@@ -3,62 +3,69 @@ using System;
 using System.Collections.Generic;
 using static Godot.GD;
 
-public partial class ExplosiveEnemy : GenericCharacterClass
+public partial class WizardInsectExplosive : GenericCharacterClass
 {
-
-    [Export] public float Speed = 150f;
-    [Export] public float Gravity = 800f;
-    [Export] public Vector2 Direction = Vector2.Right;
-
+    /* 
+		[Export] public float Speed = 150f;
+		[Export] public float Gravity = 800f;
+		[Export] public Vector2 Direction = Vector2.Left;
+	 */
     private List<GenericCharacterClass> victims = new List<GenericCharacterClass>();
     private List<GenericCharacterClass> victims_Explosion = new List<GenericCharacterClass>();
 
-    private double health = 100;
-    private bool dead = false;
+    private double health = 20;
     private double red_time;
+    private bool dead = false;
+    private AudioStreamPlayer DeathSFX;
+    private AudioStreamPlayer DamageSFX;
+    private double timeuntilattack;
+    static double attacktime = 3;
+
     private double TimeSinceFlash;
     private bool Flash = false;
 
-    private AudioStreamPlayer DeathSFX;
-    private AudioStreamPlayer DamageSFX;
-
-    private RayCast2D _wallRay;
-    private RayCast2D _groundRay;
+    /* 	private RayCast2D _wallRay;
+				private RayCast2D _groundRay; */
 
     private Sprite2D _sprite;
+
+    private bool damaging = false;
+
+
 
     public override void _Ready()
     {
         GetEnemyAudioNodes();
-        _groundRay = GetNode<RayCast2D>("GroundRay");
+        //_groundRay = GetNode<RayCast2D>("GroundRay");
         _sprite = GetNodeOrNull<Sprite2D>("Sprite2D");
 
         var originalMaterial = Material;
         var uniqueMaterial = (Material)originalMaterial.Duplicate();
         Material = uniqueMaterial;
+        timeuntilattack = attacktime;
     }
 
 
     public override void _PhysicsProcess(double delta)
     {
 
-        Vector2 velocity = Velocity;
+        /* Vector2 velocity = Velocity;
 
-        velocity.X = Direction.X * Speed;
-        velocity.Y += Gravity * (float)delta;
-        Velocity = velocity;
-        MoveAndSlide();
+		velocity.X = Direction.X * Speed;
+		velocity.Y += Gravity * (float)delta;
+		Velocity = velocity;
+		MoveAndSlide();
 
-        _groundRay.TargetPosition = Direction * 100 + Vector2.Down * 120;
-        _groundRay.ForceRaycastUpdate();
+		_groundRay.TargetPosition = Direction * 100 + Vector2.Down * 120;
+		_groundRay.ForceRaycastUpdate();
 
 
-        if (!_groundRay.IsColliding())
-        {
-            FlipDirection();
+		if (!_groundRay.IsColliding())
+		{
+			FlipDirection();
 
-        }
-
+		}
+ */
         //Print("red time");
         //Print(red_time);
 
@@ -78,6 +85,17 @@ public partial class ExplosiveEnemy : GenericCharacterClass
 
         deal_damage();
 
+        timeuntilattack -= delta;
+        if (timeuntilattack <= 0)
+        {
+            timeuntilattack = attacktime;
+            if (Math.Abs(GetNode<Player>("/root/Game/%Player").GlobalPosition.X - this.GlobalPosition.X) < 1000 && Math.Abs(GetNode<Player>("/root/Game/%Player").GlobalPosition.Y - this.GlobalPosition.Y) < 500)
+            {
+                attack();
+            }
+
+        }
+
         if (Flash && TimeSinceFlash > 0.2)
         {
             if ((int)(Material as ShaderMaterial).GetShaderParameter("damage_shader_int") == 0)
@@ -91,6 +109,8 @@ public partial class ExplosiveEnemy : GenericCharacterClass
             TimeSinceFlash = 0;
         }
         TimeSinceFlash += delta;
+
+
     }
 
     public virtual void GetEnemyAudioNodes()
@@ -99,35 +119,40 @@ public partial class ExplosiveEnemy : GenericCharacterClass
         DamageSFX = GetNode<AudioStreamPlayer>("/root/Game/%standart_enemy_damage_sfx");
     }
 
+    /* 
+		private void _on_wall_hit(Node2D body)
+		{
+			FlipDirection();
+		}
 
-    private void _on_wall_hit(Node2D body)
-    {
-        FlipDirection();
-    }
 
-
-    private void FlipDirection()
-    {
-        if (IsOnFloor())
-        {
-            Direction = -Direction;
-            var damageArea = GetNode<Node2D>("damage_area/CollisionShape2D");
-            damageArea.Position = new Vector2(-damageArea.Position.X, damageArea.Position.Y);
-            var WallBox = GetNode<Node2D>("wall_box/CollisionShape2D");
-            WallBox.Position = new Vector2(-WallBox.Position.X, WallBox.Position.Y);
-            if (_sprite != null)
-            {
-                _sprite.FlipH = !_sprite.FlipH;
-            }
-        }
-    }
+		private void FlipDirection()
+		{
+			if (IsOnFloor())
+			{
+				Direction = -Direction;
+				var damageArea = GetNode<Node2D>("damage_area/CollisionShape2D");
+				damageArea.Position = new Vector2(-damageArea.Position.X, damageArea.Position.Y);
+				var WallBox = GetNode<Node2D>("wall_box/CollisionShape2D");
+				WallBox.Position = new Vector2(-WallBox.Position.X, WallBox.Position.Y);
+				if (_sprite != null)
+				{
+					_sprite.FlipH = !_sprite.FlipH;
+				}
+			}
+		}
+		*/
 
     public void deal_damage()
     {
-        for (int i = 0; i < victims.Count; i++)
+        if (damaging)
         {
-            victims[i].take_damage(1);
+            for (int i = 0; i < victims.Count; i++)
+            {
+                victims[i].take_damage(1);
+            }
         }
+
     }
 
     public override void take_damage(double damage)
@@ -156,7 +181,6 @@ public partial class ExplosiveEnemy : GenericCharacterClass
         if (victim as GenericCharacterClass != null && victim != this)
         {
             victims.Add(victim as GenericCharacterClass);
-            Print("Sorry" + victim);
         }
     }
 
@@ -165,11 +189,19 @@ public partial class ExplosiveEnemy : GenericCharacterClass
         if (victim as GenericCharacterClass != null)
         {
             victims.Remove(victim as GenericCharacterClass);
-            Print("Not today" + victim);
         }
     }
 
-    private async void killed()
+    private async void attack()
+    {
+        GetNode<Sprite2D>("damage_area/Sprite2D").Visible = true;
+        damaging = false;
+        GetNode<Node2D>("damage_area").GlobalPosition = GetNode<Player>("/root/Game/%Player").GlobalPosition;
+        await ToSignal(GetTree().CreateTimer(0.5), SceneTreeTimer.SignalName.Timeout);
+        damaging = true;
+    }
+    
+     private async void killed()
     {
         var OverlayRef = GetNode("/root/Game/%overlay") as V2Overlay;
         OverlayRef.AddPoints(10000);
@@ -211,4 +243,6 @@ public partial class ExplosiveEnemy : GenericCharacterClass
             Print("bye" + victim);
         }
     }
+	
+	
 }
