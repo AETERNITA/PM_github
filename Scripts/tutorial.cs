3b436df252fe8b14d269b5a31dcd604547bf01d5
TToT999 <ttot999@web.de>
2025-07-09 16:42:17 +0200

    Tutorial

--- Scripts/tutorial.cs
+++ Scripts/tutorial.cs
@@ -1,7 +1,12 @@
 using Godot;
 using System;
 
-public partial class tutorial : Node
+public partial class tutorial : Control
 {
-	
+	[Export] Player player;
+	public override void _Process(){
+	if (player.GiveX()>5500){
+		spawn
+	}
+	}
 }
